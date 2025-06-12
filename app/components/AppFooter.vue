79c77269e836af440e7bc3eef1e001ff59a283f5
Bradley Cainday <bradleycainday@gmail.com>
2025-06-12 10:26:01 +0800

    create reusable nav bar and footer components

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-footer color="grey-lighten-4" app class="pa-3">
+    <v-container class="d-flex justify-center align-center">
+      <span class="text-grey-darken-1 mr-2">© {{ new Date().getFullYear() }} SpaceX Explorer</span>
+      <v-divider vertical class="mx-2" />
+      <a href="https://www.spacex.com/" target="_blank" class="text-primary text-decoration-underline">SpaceX</a>
+    </v-container>
+  </v-footer>
+</template>
+
+<script setup>
+// No additional logic needed
+</script>
+
+<style scoped>
+.v-footer {
+  border-top: 1px solid #e0e0e0;
+}
+</style> 
---
+++ app/components/AppNavbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar color="primary" dark elevate-on-scroll app>
+    <v-container class="d-flex align-center">
+      <NuxtLink to="/" class="text-white text-h6 text-decoration-none mr-8">
+        <v-icon icon="mdi-rocket-launch" class="mr-2" /> SpaceX Explorer
+      </NuxtLink>
+      <v-spacer />
+      <v-btn to="/" variant="text" class="text-white" exact>Home</v-btn>
+      <v-btn to="/launches" variant="text" class="text-white">Launches</v-btn>
+      <v-btn to="/rockets" variant="text" class="text-white">Rockets</v-btn>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script setup>
+// No additional logic needed
+</script>
+
+<style scoped>
+.v-app-bar {
+  border-bottom: 1px solid #1976d2;
+}
+</style> 
